0347122763e42c2f2528ef7392e70d41492c607f
Tristan Weeden <weedent1@gmatc.matc.edu>
2021-05-11 20:19:58 -0500

    added styled components

---
+++ dinner-decider/src/pages/RandomRecipie.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import { Title } from '../styles/RandomRecipeStyles'
+function RandomRecipe() {
+  return (
+    <div>
+        <Title>Hello</Title>
+    </div>
+  );
+}
+
+export default RandomRecipe;
